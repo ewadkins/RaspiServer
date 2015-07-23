982b5975676bed346e271c075478b9d585247e04
ewadkins <ewadkins83@gmail.com>
2015-07-22 23:11:23 -0400

    Changes

--- host.js
+++ host.js
@@ -13,22 +13,20 @@ app.use(express.static(__dirname + '/' + settings.server.fileLocation));
 app.get('/', function (req, res) {
 	var ip = req.ip;
 	console.log('Request received from ' + ip);
-	var files = fs.readdirSync(settings.server.imageDirectory);
-	console.log(files);
+	var files = fs.readdirSync(settings.server.fileLocation + '/' + settings.server.imageDirectory);
 	var html = '<!DOCTYPE html>';
 	html += '<head>';
 	html += '<title>Memes ftw</title>';
 	html += '</head>';
 	html += '<body>';
-	html += '<div>';
+	html += '<div style="padding:5pt">';
 	for (var i = 0; i < files.length; i++) {
-		html += '<img src="' + settings.server.imageDirectory + '/' + files[i] + '">';
+		html += '<img style="padding:3pt" src="' + settings.server.imageDirectory + '/' + files[i] + '">';
 	}
 	html += '</div>';
 	html += '</body>';
 	html += '</html>';
 	
-	console.log(html);
 	res.send(html);
 	//res.redirect(settings.server.index);
 });
